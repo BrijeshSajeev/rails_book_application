4e7f1397d63bd48f4c00deed2c177713718b52b2
brijeshsajeev <brijesh00300@gmail.com>
2024-06-17 23:30:30 +0530

    Add new and create method to create new object

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,23 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_param)
+    if @book.save
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def book_param
+    params.require(:book).permit(:name,:author,:price,:year,:genre)
+  end
+
 end
