31c4c8e257db595d198eb1ff9512c01939f41dca
brijeshsajeev <brijesh00300@gmail.com>
2024-06-18 10:01:55 +0530

    Add validation function for every field to avoid error

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :name, presence: true
+  validates :year, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :price, presence: true
 end
