a5edbae04fcdf0cfb8a13e5c4a857a0d77febc0e
brijeshsajeev <brijesh00300@gmail.com>
2024-06-21 11:49:16 +0530

    Add active association between author and book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,10 @@
 class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  
+  belongs_to :author
+
   validates :name, presence: true
   validates :year, presence: true
-  validates :author, presence: true
+
   validates :genre, presence: true
   validates :price, presence: true
 end
