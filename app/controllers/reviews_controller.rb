ced4d1d9f08327b5be79476a31cfb2f354a469a1
brijeshsajeev <brijesh00300@gmail.com>
2024-06-21 22:49:51 +0530

    Refactor: add before_action to set the book to the reviews controller
    - add set_book function for create, destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,22 @@
 class ReviewsController < ApplicationController
+  before_action :set_book, only: [:create, :destroy]
+  
   def create
-    @book = Book.find(params[:book_id])
     @review = @book.reviews.create(review_params)
     redirect_to book_path(@book)
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
     @review = @book.reviews.find(params[:id])
     @review.destroy
     redirect_to book_path(@book), status: :see_other
   
   end
   private
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
   def review_params
     params.require(:review).permit(:content, :rating)
   end
