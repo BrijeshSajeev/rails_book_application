971fca5734c9e9b8a0494c0bd01bc3889fc4c764
brijeshSajeev <brijesh00300@gmail.com>
2024-07-06 11:27:10 +0530

    Generate a Migration for Adding Image Column in Books model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_05_150331) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_06_055639) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_05_150331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id", null: false
+    t.string "image"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
