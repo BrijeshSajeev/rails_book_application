9319f5ca06e40961bed03f7b7375e708fe830a60
brijeshSajeev <brijesh00300@gmail.com>
2024-07-09 20:42:32 +0530

    Add validations for the image size and content type

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,5 +9,6 @@ class Book < ApplicationRecord
   validates :price, presence: true
 
   has_one_attached :image_new
+  validates :image_new, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 2.megabytes, message: 'is too large. Maximum size allowed is 2MB.' }
   # mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_one_attached :image_new
+  validates :image_new, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 2.megabytes, message: 'is too large. Maximum size allowed is 2MB.' }
   # mount_uploader :image, ImageUploader
 
   validates :name, presence: true, uniqueness: true
